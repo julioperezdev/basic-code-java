266faa458fe0c091919929c89b4265bd58c09cff
julioperezdev <protobot.dev@gmail.com>
2021-08-14 11:27:25 -0300

    generate main type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,46 @@
 package com.company;
 
+import com.company.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //main data type
+
+        //numeric
+        final int intNumber = 123;
+        final long longNumber = 123L;
+        final Integer integerReferenceNumber = 123;
+        final Long longReferenceNumber = 123L;
+
+        //string
+        final String StringReference = "abc";
+        final Character charType;
+        final char charValue;
+
+        //boolean
+        final boolean booleanTrue = true;
+        final boolean booleanFalse = false;
+        final Boolean booleanReferenceTrue;
+        final Boolean booleanReferenceFalse;
+
+        //Arrays
+        final int[] intArrayNumber = {1,2,3,4,100,32,453,2};
+        final String[] StringArrayReference = {"Julio", "Andres", "Maria", "Andres"};
+        final boolean[] booleanArray = {true, false, true, true};
+
+        //Object
+        final Student studentObject = new Student("Julio", 25);
+
+        //Object Arrays
+        final Student[] studentArrayObject = {new Student("Julio", 25),new Student("Maria", 60)};
+        final List<Student> studentList = new ArrayList<>();
+
+        //others
+        final byte byteValue;
+        final byte[] byteArrayValue;
     }
 }
---
+++ src/com/company/model/Student.java
@@ -0,0 +1,12 @@
+package com.company.model;
+
+public class Student {
+
+    private final String nameStudent;
+    private final int ageStudent;
+
+    public Student(String nameStudent, int ageStudent) {
+        this.nameStudent = nameStudent;
+        this.ageStudent = ageStudent;
+    }
+}
