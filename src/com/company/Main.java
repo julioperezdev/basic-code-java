00d822639e400eedee3eec0e8700967ed6adb50a
julioperezdev <protobot.dev@gmail.com>
2021-08-14 10:59:24 -0300

    setup coding

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        final String name = "Gabriel";
-        final int nameNumber = 35;
-        //Seguridad instanciaSeguridad = new Seguridad();
 
-        //System.out.println(name);
-        //System.out.println(nameNumber);
-
-
-        //System.out.println(instanciaSeguridad.devolverAfirmativos(name, nameNumber));
 
     }
 }
